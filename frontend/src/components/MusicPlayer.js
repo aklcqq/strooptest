ec263e648ab3691533ea6e65f254461d82a679fc
root <root@debian.vcn04011836.oraclevcn.com>
2024-04-17 12:16:00 -0400

    hide control bar for silent

--- frontend/src/components/MusicPlayer.js
+++ frontend/src/components/MusicPlayer.js
@@ -12,10 +12,20 @@ function getSongPath(genre) {
   return genreToPathMapping[genre] || '/songs/silent.mp3';
 }
 
-const MusicPlayer = ({ song, autoPlay, loop }) => (
-  // remove controls to hide control panel
-  <audio controls src={getSongPath(song)} autoPlay={autoPlay} loop={loop} />
+const MusicPlayer = ({ song, autoPlay, loop }) => {
+  // Determine if the song is 'silent' to decide on showing controls
+  const showControls = song !== 'silent';
 
-);
+  return (
+    <audio
+      src={getSongPath(song)}
+      autoPlay={autoPlay}
+      loop={loop}
+      // Conditionally render the controls attribute based on showControls
+      {...(showControls ? { controls: true } : {})}
+    />
+  );
+};
+
 
 export default MusicPlayer;
